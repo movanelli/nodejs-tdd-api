9c2a3dd62b867291b1ff5bc6ae12ec17d0eb8d06
Moises Meirelles <mshmeirelles@gmail.com>
2016-12-20 16:07:11 -0200

    CRUD integration tests

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
 import config from './config/config';
 import datasource from './config/datasource';
 
@@ -8,12 +10,37 @@ const app = express();
 app.config = config;
 app.datasource = datasource(app);
 app.set('port', 7000);
+app.use(bodyParser.json());
 
 const Book = app.datasource.models.Book;
-app.route('/books').get((req, res) => {
-  Book.findAll({})
-    .then(result => res.json(result))
-    .catch(err => res.status(412));
-});
+
+app.route('/books')
+  .get((req, res) => {
+    Book.findAll({})
+      .then(result => res.json(result))
+      .catch(err => res.status(412));
+  })
+  .post((req, res) => {
+    Book.create(req.body)
+      .then(result => res.json(result))
+      .catch(err => res.status(412));
+  });
+
+app.route('/books/:id')
+  .get((req, res) => {
+    Book.findOne({where: req.params})
+      .then(result => res.json(result))
+      .catch(err => res.status(412));
+  })
+  .put((req, res) => {
+    Book.update(req.body, {where: req.params})
+      .then(result => res.json(result))
+      .catch(err => res.status(412));
+  })
+  .delete((req, res) => {
+    Book.destroy({where: req.params})
+      .then(result => res.sendStatus(204))
+      .catch(err => res.status(412));
+  });
 
 export default app;
--- test/integration/app.js
+++ test/integration/app.js
@@ -17,14 +17,91 @@ describe('Routes Books', () => {
   describe('Route GET /books', () => {
     it('must return a list of books', done => {
 
-      request.get('/books').end((err, res) => {
+      request
+        .get('/books')
+        .end((err, res) => {
 
-        expect(res.body[0].id).to.be.eql(defaultBook.id);
-        expect(res.body[0].name).to.be.eql(defaultBook.name);
+          expect(res.body[0].id).to.be.eql(defaultBook.id);
+          expect(res.body[0].name).to.be.eql(defaultBook.name);
 
-        done(err);
-      });
+          done(err);
+        });
 
     });
   });
+
+  describe('Route GET /books/{id}', () => {
+    it('must return a book', done => {
+
+      request
+        .get('/books/1')
+        .end((err, res) => {
+
+          expect(res.body.id).to.be.eql(defaultBook.id);
+          expect(res.body.name).to.be.eql(defaultBook.name);
+
+          done(err);
+        });
+
+    });
+  });
+
+  describe('Route POST /books', () => {
+    it('must create a book', done => {
+
+      const newBook = {
+        id: 2,
+        name: 'New Book'
+      };
+
+      request
+        .post('/books')
+        .send(newBook)
+        .end((err, res) => {
+
+          expect(res.body.id).to.be.eql(newBook.id);
+          expect(res.body.name).to.be.eql(newBook.name);
+
+          done(err);
+        });
+
+    });
+  });
+
+  describe('Route PUT /books/{id}', () => {
+    it('must update a book', done => {
+
+      const updatedBook = {
+        id: 1,
+        name: 'Updated Book'
+      };
+
+      request
+        .put('/books/1')
+        .send(updatedBook)
+        .end((err, res) => {
+
+          expect(res.body).to.be.eql([1]);
+
+          done(err);
+        });
+
+    });
+  });
+
+  describe('Route DELETE /books/{id}', () => {
+    it('must delete a book', done => {
+
+      request
+        .delete('/books/1')
+        .end((err, res) => {
+
+          expect(res.statusCode).to.be.eql(204);
+
+          done(err);
+        });
+
+    });
+  });
+
 });
