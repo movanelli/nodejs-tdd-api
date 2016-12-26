cf49d10adb7f203527392c4eb6265bf7c298261a
Moises Meirelles <mshmeirelles@gmail.com>
2016-12-26 14:25:45 -0200

    Contract tests

---
+++ test/contract/contracts/book.js
@@ -0,0 +1,111 @@
+describe('Routes Books', () => {
+  const Book = app.datasource.models.Book;
+  const defaultBook = {
+    id: 1,
+    name: 'Default Book',
+  };
+
+  beforeEach((done) => {
+    Book
+      .destroy({ where: {} })
+      .then(() => Book.create(defaultBook))
+      .then(() => {
+        done();
+      });
+  });
+
+  describe('Route GET /books', () => {
+    it('must return a list of books', (done) => {
+      const booksList = Joi.array().items(Joi.object().keys({
+        id: Joi.number(),
+        name: Joi.string(),
+        created_at: Joi.date().iso(),
+        updated_at: Joi.date().iso(),
+      }));
+
+      request
+        .get('/books')
+        .end((err, res) => {
+          joiAssert(res.body, booksList);
+
+          done(err);
+        });
+    });
+  });
+
+  describe('Route GET /books/{id}', () => {
+    it('must return a book', (done) => {
+      const book = Joi.object().keys({
+        id: Joi.number(),
+        name: Joi.string(),
+        created_at: Joi.date().iso(),
+        updated_at: Joi.date().iso(),
+      });
+
+      request
+        .get('/books/1')
+        .end((err, res) => {
+          joiAssert(res.body, book);
+
+          done(err);
+        });
+    });
+  });
+
+  describe('Route POST /books', () => {
+    it('must create a book', (done) => {
+      const book = Joi.object().keys({
+        id: Joi.number(),
+        name: Joi.string(),
+        created_at: Joi.date().iso(),
+        updated_at: Joi.date().iso(),
+      });
+
+      const newBook = {
+        id: 2,
+        name: 'New Book',
+      };
+
+      request
+        .post('/books')
+        .send(newBook)
+        .end((err, res) => {
+          joiAssert(res.body, book);
+
+          done(err);
+        });
+    });
+  });
+
+  describe('Route PUT /books/{id}', () => {
+    it('must update a book', (done) => {
+      const updatedBook = {
+        id: 1,
+        name: 'Updated Book',
+      };
+
+      const updatedCount = Joi.array().items(1);
+
+      request
+        .put('/books/1')
+        .send(updatedBook)
+        .end((err, res) => {
+          joiAssert(res.body, updatedCount);
+
+          done(err);
+        });
+    });
+  });
+
+  describe('Route DELETE /books/{id}', () => {
+    it('must delete a book', (done) => {
+      request
+        .delete('/books/1')
+        .end((err, res) => {
+          expect(res.statusCode).to.be.eql(204);
+
+          done(err);
+        });
+    });
+  });
+});
